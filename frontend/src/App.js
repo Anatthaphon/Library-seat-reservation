75430af15884bcf6d2e84ecd0fbabe91ef29f062
Anatthaphon <anatthaphon.w@ku.th>
2025-12-25 02:40:04 +0700

    App update ปรับปรุงให้หน้า planning ไป  SeatMap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './styles/App.css';
 
 // Pages
 import Planning from './pages/Planning';
+import SeatMap from './pages/SeatMap';
 
 // Components
 import Navbar from './components/Navbar';
@@ -17,12 +18,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Navigate to="/planning" replace />} />
             <Route path="/planning" element={<Planning />} />
-            {/* TODO: Implement authentication routes */}
-            {/* <Route path="/login" element={<Login />} /> */}
-            {/* <Route path="/register" element={<Register />} /> */}
-            {/* <Route path="/home" element={<Home />} /> */}
-            {/* TODO: Implement seat reservation system */}
-            {/* <Route path="/seats" element={<SeatReservation />} /> */}
+
+            {/* Seat map */}
+            <Route path="/seat-map" element={<SeatMap />} />
+            <Route path="/seatmap" element={<SeatMap />} />
+
+            {/* other routes... */}
           </Routes>
         </div>
       </div>
