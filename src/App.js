608d7268807c884307b0f74d66405b1a70106add
Gegee-notavailable <nattanicha.chimruang48@gmail.com>
2026-01-26 01:20:03 +0700

    dashboard

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './styles/App.css';
+
+// Pages
+import Planning from './pages/Planning';
+import SeatMap from './pages/SeatMap';
+import Reserve from './pages/reserve';
+import Dashboard from "./pages/dashboard";
+// Components
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Navigate to="/planning" replace />} />
+            <Route path="/planning" element={<Planning />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+        
+            {/* Seat map */}
+            <Route path="/seat-map" element={<SeatMap />} />
+            <Route path="/seatmap" element={<SeatMap />} />
+             <Route path="/reserve" element={<Reserve />} />
+
+            {/* other routes... */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
